2eaae263a97847133407ca4ed302ba5b2ffd71b0
Mikhail <35026973+MaiklGureev@users.noreply.github.com>
2020-06-25 21:39:15 +0400

    Add ExampleUnitTest

--- app/src/test/java/com/example/diaryofmikhailgureev/ExampleUnitTest.java
+++ app/src/test/java/com/example/diaryofmikhailgureev/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.example.diaryofmikhailgureev;
 
+import com.example.diaryofmikhailgureev.entities.Task;
+
 import org.junit.Test;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 import static org.junit.Assert.*;
 
@@ -12,6 +17,14 @@ import static org.junit.Assert.*;
 public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+        Calendar calendarStart = new GregorianCalendar(2020,01,01,10,00,00);
+        Calendar calendarFinish = new GregorianCalendar(2020,01,01,12,00,00);
+        Task task = new Task(123,"MyTask","MyDescription",calendarStart,calendarFinish);
+        assertEquals("MyTask", task.getTitle());
+        assertEquals("MyDescription", task.getDescription());
+        assertEquals("Задача: MyTask", task.getFormattedTitle());
+        assertEquals("01.02.2020 10:00 - 01.02.2020 12:00", task.getFormattedDateAndTime());
+        assertEquals("10:00 - 12:00", task.getFormattedTime());
+
     }
 }
